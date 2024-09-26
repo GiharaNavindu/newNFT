1d115fb3d522e9fe2a6a87b925ff88495f17f085
P.A.G.N.  Rathnaweeera <rathnaweera_pagn_e23@engug.ruh.ac.lk>
2024-09-26 11:33:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174','https://new-nft-front.vercel.app/'],
+  origin: ['http://localhost:5173', 'http://localhost:5174','https://new-nft-front.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
