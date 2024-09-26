f381e99977b46b391436d52e99b8f86f034fb3ea
P.A.G.N.  Rathnaweeera <rathnaweera_pagn_e23@engug.ruh.ac.lk>
2024-09-26 10:54:03 +0530

    Update Register.jsx

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,52 +1,3 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import { useNavigate } from 'react-router-dom';
-
-// const Register = () => {
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [error, setError] = useState(null);
-//   const navigate = useNavigate();
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       await axios.post('http://localhost:4000/api/auth/register', { username, password });
-//       console.log("registration is successfull"+username);
-//       alert('User registered successfully');
-      
-//       navigate('/login'); // Redirect to login page after successful registration
-//     } catch (err) {
-//       console.error(err);
-//       setError('Registration failed'); // Set error state
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           type="text"
-//           value={username}
-//           onChange={(e) => setUsername(e.target.value)}
-//           placeholder="Username"
-//           required
-//         />
-//         <input
-//           type="password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//           placeholder="Password"
-//           required
-//         />
-//         <button type="submit" style={{ backgroundColor: 'yellow', color: 'white' }}>Register</button>
-//       </form>
-//       {error && <p style={{ color: 'red' }}>{error}</p>} {/* Display error message */}
-//     </div>
-//   );
-// };
-
-// export default Register;
 
 
 
@@ -75,7 +26,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:4000/api/auth/register', { username, password });
+      await axios.post('https://new-nft-gules.vercel.app//api/auth/register', { username, password });
       console.log("Registration is successful for user:", username);
       alert('User registered successfully');
       navigate('/login'); // Redirect to login page after successful registration
