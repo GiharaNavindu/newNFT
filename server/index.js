6bb751cfc43c7736ffb4b4fa321a588c52278098
P.A.G.N.  Rathnaweeera <rathnaweera_pagn_e23@engug.ruh.ac.lk>
2024-09-26 12:48:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,12 @@
-
-
-
 const express = require('express');
 const cors = require('cors');
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv'); // Uncomment this line
 const mongoose = require('mongoose');
 const orderRoutes = require('./routes/orders');
 const authRoutes = require('./routes/auth');
 const nftRoutes = require('./routes/nftRoutes');
 
-dotenv.config();
+dotenv.config(); // This should work now
 require('dotenv').config({ path: '.env' });
 
 const app = express();
@@ -18,7 +15,6 @@ console.log('connected1');
 app.use(cors({
   // origin: ['http://localhost:5173', 'http://localhost:5174','https://new-nft-front.vercel.app'],
   origin: ['https://new-nft-front.vercel.app'],
-  console.log('connected2');
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
@@ -37,11 +33,9 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/nfts', nftRoutes);
 
-
-
 console.log('DB_URI:', process.env.DB_URI);
 
-mongoose.connect(process.env.DB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(process.env.PORT || 4000, () => {
       console.log('connected to the db & listening on port', process.env.PORT || 4000);
