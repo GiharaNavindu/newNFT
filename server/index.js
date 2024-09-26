9832f791ea58f618caa08d407f3272219680b3ff
P.A.G.N.  Rathnaweeera <rathnaweera_pagn_e23@engug.ruh.ac.lk>
2024-09-26 12:04:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ console.log('connected');
 
 app.use(cors({
   // origin: ['http://localhost:5173', 'http://localhost:5174','https://new-nft-front.vercel.app'],
-  origin: 'https://new-nft-front.vercel.app/',
+  origin: ['https://new-nft-front.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
