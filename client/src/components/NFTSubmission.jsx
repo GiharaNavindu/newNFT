c9221eac7b567b4110e9d5fb15fe808731502f84
P.A.G.N.  Rathnaweeera <rathnaweera_pagn_e23@engug.ruh.ac.lk>
2024-10-12 15:39:10 +0530

    Update NFTSubmission.jsx

--- client/src/components/NFTSubmission.jsx
+++ client/src/components/NFTSubmission.jsx
@@ -39,7 +39,7 @@ const NFTSubmission = () => {
     const token = localStorage.getItem('token');
 
     try {
-      const res = await axios.put(`https://new-nft-gules.vercel.app/api/nfts/${updatedNft._id}`, updatedNft, {
+      const res = await axios.put(`https://backend-pi-wheat.vercel.app/api/nfts/${updatedNft._id}`, updatedNft, {
         headers: {
           Authorization: `Bearer ${token}`,
           'Content-Type': 'application/json',
@@ -56,7 +56,7 @@ const NFTSubmission = () => {
     const token = localStorage.getItem('token');
 
     try {
-      await axios.delete(`https://new-nft-gules.vercel.app/api/nfts/${id}`, {
+      await axios.delete(`https://backend-pi-wheat.vercel.app/api/nfts/${id}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
