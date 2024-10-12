575e43cb04031e55746fef9e8ac1ece6eba9e7bf
P.A.G.N.  Rathnaweeera <rathnaweera_pagn_e23@engug.ruh.ac.lk>
2024-10-12 15:38:21 +0530

    Update NFTForm.jsx

--- client/src/components/NFTForm.jsx
+++ client/src/components/NFTForm.jsx
@@ -100,7 +100,7 @@ const NFTForm = () => {
       formData.append('price', price);
       formData.append('image', image); // Append the file directly
   
-      const response = await axios.post('https://new-nft-gules.vercel.app/api/nfts', formData, {
+      const response = await axios.post('https://backend-pi-wheat.vercel.app/api/nfts', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
